d501448d521cbe59b9d4ce7a69e367556171e442
eason <eason920@gmail.com>
2020-09-28 18:19:08 +0800

    1090928

--- source/js/mylesson.js
+++ source/js/mylesson.js
@@ -1,6 +1,26 @@
 $(()=>{
-	console.log('got');
+	$(function () {
+
+	});
+
+
+$( "#datepicker" ).datepicker({
+	showWeek: true,
+	showOtherMonths: true,
+	numberOfMonths: 2
+});
+
+const thisWeek = $('.ui-datepicker-today').siblings().eq(0).text();
+const nextWeek = thisWeek + 1;
+
+console.log(`
+%cthis week is ${thisWeek}
+`, 'color:yellow');
+
+
 	$('#contact, #lbmasker').click(function(){
 		$('#lbmasker, #lb').toggleClass('open');
 	});
+
+
 });
